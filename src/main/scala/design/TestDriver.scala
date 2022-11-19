651471fe0d4426eac09e47ce8a8ac616c26d32f2
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-19 16:40:10 -0500

    Specify run by class definition.

--- src/main/scala/design/TestDriver.scala
+++ src/main/scala/design/TestDriver.scala
@@ -4,14 +4,18 @@ import firrtl.options.StageMain
 import freechips.rocketchip.system.RocketChipStage
 import firrtl.stage.FirrtlMain
 
-object Driver extends App {
+object TestDriver extends App {
+
+  val config = new GemmConfig
   val c_dir = System.getProperty("user.dir")
   val hw_idr = c_dir + "/vsim/generated-src/"
-  println("the cname is " + (new exampleConfig).getClass.getCanonicalName)
-  new RocketChipStage().execute(Array("-td", hw_idr, "-T", "composer.ComposerTop", "-C", "design.exampleConfig"), Seq())
+  val full_name = config.getClass.getCanonicalName
+  val short_name = full_name.split('.').last
+  println(full_name + " " + short_name)
+  new RocketChipStage().execute(Array("-td", hw_idr, "-T", "composer.ComposerTop", "-C", full_name), Seq())
 //  run(Seq(
 //    new TargetDirAnnotation(hw_idr),
 //    new TopModuleAnnotation(Class.forName("composer.ComposerTop")),
 //    new ConfigsAnnotation(Seq("design.exampleConfig"))))
-  FirrtlMain.stage.execute(Array("-i", hw_idr + "/composer.exampleConfig.fir", "-o", hw_idr + "composer.v", "-X", "verilog"), Seq())
+  FirrtlMain.stage.execute(Array("-i", hw_idr + "/composer." + short_name + ".fir", "-o", hw_idr + "composer.v", "-X", "verilog"), Seq())
 }
