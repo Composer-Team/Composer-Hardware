0a6f6ea5c697084f575d6d6e3f403b1e5d5652a9
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-24 09:46:59 -0800

    more complete test case

--- src/main/scala/design/Gemm.scala
+++ src/main/scala/design/Gemm.scala
@@ -433,5 +433,6 @@ class WithGemm(withNCores: Int,
 })
 
 class GemmConfig extends Config(
-  new WithGemm(1, GemmParam(4, 16, 2, 2, 1024)) ++ new WithComposer() ++ new WithAWSMem(1)
+  new WithGemm(1, GemmParam(4, 16, 2, 2, 1024)) ++ new WithVectorAdder(1, 16) ++
+    new WithALUs(1) ++ new WithComposer() ++ new WithAWSMem(1)
 )
