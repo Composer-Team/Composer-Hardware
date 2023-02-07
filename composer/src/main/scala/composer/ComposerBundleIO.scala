cbb13a04564008a27535522927ad3d850fa8b246
brendanpeercy <nbabsp@gmail.com>
2023-02-07 09:22:28 -0500

    direct passthrough working

--- composer/src/main/scala/composer/ComposerBundleIO.scala
+++ composer/src/main/scala/composer/ComposerBundleIO.scala
@@ -3,61 +3,54 @@ package composer
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 
-class ComposerBundleIO[T1 <: Bundle, T2 <: Bundle](ccio: ComposerCoreIO, bundleIn: T1, bundleOut: T2)(implicit p: Parameters) extends Module {
+class ComposerBundleIO[T1 <: Bundle, T2 <: Bundle](bundleIn: T1, bundleOut: T2, composerCoreParams: ComposerCoreParams)(implicit p: Parameters) extends Module {
 
   val cio = IO(new ComposerCoreIO)
   val io = IO(new CustomIO(bundleIn.cloneType, bundleOut.cloneType))
-  io.req.bits.elements.foreach { case (string, data) => data := 0.U }
-  io.req.valid := false.B
-  io.resp.ready := false.B
-  //  io.resp.bits.elements.foreach { case (string, data) => data := 0.U }
-  //  io.busy := false.B
-  //  io.req.ready := false.B
-  //  io.resp.valid := false.B
 
-  cio.busy := io.req.ready
+  //zero initialize values
+  io.req.bits.elements.foreach { case (_, data) => data := 0.U }
+  cio.resp.bits.elements.foreach { case (_, data) => data := 0.U }
+
+  //System initialized values
+  cio.resp.bits.system_id := composerCoreParams.system_id.U
+  cio.resp.bits.core_id := composerCoreParams.core_id.U
+
+  //Only works for direct pass-through
+  io.req.valid := cio.req.valid
+  io.resp.ready := cio.resp.ready
+  cio.busy := io.busy
   cio.req.ready := io.req.ready
   cio.resp.valid := io.resp.valid
 
-  ccio.req.valid := false.B
-  ccio.resp.bits.data := 0.U
-  ccio.req.bits.rs1 := 0.U
+//  //Direct pass-through
+//  when(io.req.fire) {
+//    io.req.bits.getElements(2) := cio.req.bits.inst.rs1
+//    io.req.bits.getElements(1) := cio.req.bits.rs1
+//    io.req.bits.getElements(0) := cio.req.bits.rs2
+//  }
+
+  when(io.resp.fire) {
+    cio.resp.bits.data := io.resp.bits.getElements(0)
+  }
 
 
   when(cio.req.fire) {
-    var counter1 = 0
-    var counter2 = 0
+    var counter = 0
     for(i <- io.req.bits.getElements.indices) {
       val nextElement = io.req.bits.getElements(i)
-      val start1 = counter1
-      val start2 = counter2
-      val end1 = start1 + nextElement.getWidth
-      val end2 = start2 + nextElement.getWidth
+      val start = counter
+      val end = start + nextElement.getWidth
 
       val userBools = VecInit(nextElement.asUInt.asBools)
 
-      if (end1 < cio.req.bits.rs1.getWidth) {
-
-        val composerBools = VecInit(cio.req.bits.rs1.asBools)
-
-        for(j <- 0 until nextElement.getWidth){
-          userBools(j) := composerBools(start1 + j)
-        }
-        nextElement := userBools.asUInt
-
-//        nextElement := cio.req.bits.rs1
-        counter1 = end1
-
-      } else if (end2 < cio.req.bits.rs2.getWidth) {
+      if (end < cio.req.bits.rs2.getWidth) {
         val composerBools = VecInit(cio.req.bits.rs2.asBools)
-
         for(j <- 0 until nextElement.getWidth){
-          userBools(j) := composerBools(start1 + j)
+          userBools(j) := composerBools(start + j)
         }
         nextElement := userBools.asUInt
-
-//        nextElement := cio.req.bits.rs2
-        counter2 = end2
+        counter = end
       }
     }
   }
