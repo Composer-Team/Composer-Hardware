283c8e11479ce77aa0851118ae3f6d5ea8b3e1c9
Chris Kjellqvist <chriskjellqvist@gmail.com>
2025-04-04 10:21:26 -0400

    fix some reader stuff. cleanup. improve verilog output for shells.

--- src/test/scala/basic/MyAcceleratorConfig.scala
+++ src/test/scala/basic/MyAcceleratorConfig.scala
@@ -2,19 +2,12 @@ package basic
 
 import beethoven.Platforms.FPGA.Xilinx.F2.{AWSF2Platform, DMAHelperConfig, MemsetHelper, MemsetHelperConfig}
 import beethoven._
-import beethoven.common.{AccelCommand, Address, EmptyAccelResponse}
-import chisel3._
 
 class MyAcceleratorConfig extends AcceleratorConfig(List(
   AcceleratorSystemConfig(
     nCores = 1,
     name = "MyAcceleratorSystem",
-    moduleConstructor = BlackboxBuilderCustom(new AccelCommand("my_accel") {
-      val addend = UInt(32.W)
-      val vec_addr = UInt(32.W)
-      val n_eles = UInt(20.W)
-    }, EmptyAccelResponse()),
-//    moduleConstructor = ModuleBuilder(p => new MyAccelerator()(p)),
+    moduleConstructor = ModuleBuilder(p => new MyAccelerator()(p)),
     memoryChannelConfig = List(
       ReadChannelConfig("vec_in", dataBytes = 4),
       WriteChannelConfig("vec_out", dataBytes = 4))
